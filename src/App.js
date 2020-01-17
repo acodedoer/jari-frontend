2ea723a4ae59f4034139e56a8e9244c43fa9b576
msz4 <misbahu.s.z@gmail.com>
2020-01-17 13:28:42 +0000

    passed data to Proverb component

--- src/App.js
+++ src/App.js
@@ -3,36 +3,43 @@ import './App.css';
 import Proverb from './components/Proverb';
 import ApolloClient from 'apollo-boost';
 import gql from 'graphql-tag';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { useQuery } from '@apollo/react-hooks';
+import ProverbComponent from './components/Proverb';
+
 
 const client = new ApolloClient({
   uri: 'https://jari-backend.herokuapp.com/admin/api',
 });
 
-client
-  .query({
-    query: gql`
-    {
-      allProverbs{
-        proverb
-        literalTags{
-          tag
-        }
-        metaphoricalTags{
-          tag
+    function GETALLPROVERBS() {
+      const { loading, error, data } = useQuery(gql`{
+        allProverbs{
+          proverb
+          literalTags{
+            tag
+          }
+          metaphoricalTags{
+            tag
+          }
         }
       }
+      `);
+    
+      if (loading) return <p>Loading...</p>;
+      if (error) return <p>Error :(</p>;
+      console.log(data.allProverbs)
+      return data.allProverbs.map(({ proverb, literalTags, metaphoricalTags}) => (
+        <ProverbComponent proverb ={proverb} literalTags={literalTags} metaphoricalTags={metaphoricalTags} />
+      ));
     }
-    `
-  })
-  .then(result => console.log(result));
-
 
 function App() {
   return (
-    <div style={{width:"100%"}}>
-      <Proverb/>
-    </div>
-    
+    <ApolloProvider client={client}>
+      <GETALLPROVERBS/>
+    </ApolloProvider>
+       
   );
 }
 
