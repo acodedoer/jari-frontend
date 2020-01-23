3fc3c6538195215982bf1bc8722efd59b6a89a10
msz4 <misbahu.s.z@gmail.com>
2020-01-23 14:20:20 +0000

    implemented getting proverbs by tags

--- src/App.js
+++ src/App.js
@@ -1,45 +1,18 @@
 import React from 'react';
 import './App.css';
-import Proverb from './components/Proverb';
-import ApolloClient from 'apollo-boost';
-import gql from 'graphql-tag';
 import { ApolloProvider } from '@apollo/react-hooks';
-import { useQuery } from '@apollo/react-hooks';
-import ProverbComponent from './components/Proverb';
-
+import ApolloClient from 'apollo-boost';
+import DisplayProverbs from './components/DisplayProverbs'
 
 const client = new ApolloClient({
   uri: 'https://jari-backend.herokuapp.com/admin/api',
 });
 
-    function GETALLPROVERBS() {
-      const { loading, error, data } = useQuery(gql`{
-        allProverbs{
-          proverb
-          literalTags{
-            tag
-          }
-          metaphoricalTags{
-            tag
-          }
-        }
-      }
-      `);
-    
-      if (loading) return <p>Loading...</p>;
-      if (error) return <p>Error :(</p>;
-      console.log(data.allProverbs)
-      return data.allProverbs.map(({ proverb, literalTags, metaphoricalTags}) => (
-        <ProverbComponent proverb ={proverb} literalTags={literalTags} metaphoricalTags={metaphoricalTags} />
-      ));
-    }
-
 function App() {
   return (
     <ApolloProvider client={client}>
-      <GETALLPROVERBS/>
+      <DisplayProverbs/>
     </ApolloProvider>
-       
   );
 }
 
