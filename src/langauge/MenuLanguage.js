47b70f3dd5f7fe01f323f8f8e59afabff4fc0fa1
msz4 <misbahu.s.z@gmail.com>
2020-02-15 01:08:49 +0000

    added package.json

---
+++ src/langauge/MenuLanguage.js
@@ -0,0 +1,11 @@
+const ABOUT ={"hausa":"Bayanin Jari","english":"About Jari"}
+const ALLPROVERBS ={"hausa":"Nuna Duka","english":"All Proverbs"}
+const LITERALTAGS ={"hausa":"Kalmomi","english":"Literal Tags"}
+const METAPHORICALTAGS ={"hausa":"Manufofi","english":"Metaphorical Tags"}
+const LANGUAGE={"hausa":"Yare","english":"Language"}
+
+const FUTUREPLANS={"hausa":"","english":"Future Plans"}
+const CONTRIBUTORS={"hausa":"Masu ba da gudunmowa","english":"Contributors"}
+const CONTACT ={"hausa":"Ba da gudunmowa","english":"Volunteer"}
+
+export {ABOUT, ALLPROVERBS, LITERALTAGS, METAPHORICALTAGS, LANGUAGE};
